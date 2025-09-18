fb57cc7a25c51b0f07ebdc2ff889491c02d808ac
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-09-18 17:57:28 +0600

    update favicon

--- src/redux/feature/dashboard/dashboardApi.js
+++ src/redux/feature/dashboard/dashboardApi.js
@@ -22,16 +22,6 @@ const dashboardApi = baseApi.injectEndpoints({
             providesTags: ["DASHBOARD"],
         }),
 
-        // GET DASHBOARD DONOR CHART
-        getDashboardDonorChart: builder.query({
-            query: ({ year } = {}) => ({
-                url: "/meta/donor-chart-data",
-                method: "GET",
-                params: year ? { year } : {}
-            }),
-            providesTags: ["DASHBOARD"],
-        }),
-
         // GET DASHBOARD EARNING CHART
         getDashboardEarningChart: builder.query({
             query: ({ year } = {}) => ({
@@ -42,36 +32,7 @@ const dashboardApi = baseApi.injectEndpoints({
             providesTags: ["DASHBOARD"],
         }),
 
-        // GET DASHBOARD BOND CHART
-        getDashboardBondChart: builder.query({
-            query: ({ year } = {}) => ({
-                url: "/meta/bond-chart-data",
-                method: "GET",
-                params: year ? { year } : {}
-            }),
-            providesTags: ["DASHBOARD"],
-        }),
-
-
-        // GET DASHBOARD INSTITUTION CHART
-        getDashboardInstitutionChart: builder.query({
-            query: ({ year } = {}) => ({
-                url: "/meta/institution-chart-data",
-                method: "GET",
-                params: year ? { year } : {}
-            }),
-            providesTags: ["DASHBOARD"],
-        }),
-
-        // GET DASHBOARD AUDIO CHART
-        getDashboardAudioChart: builder.query({
-            query: () => ({
-                url: "/meta/audio-pie-chart",
-                method: "GET",
-            }),
-            providesTags: ["DASHBOARD"],
-        }),
     })
 })
 
-export const { useGetDashboardStatsQuery, useGetDashboardUserChartQuery, useGetDashboardDonorChartQuery, useGetDashboardEarningChartQuery, useGetDashboardBondChartQuery, useGetDashboardInstitutionChartQuery, useGetDashboardAudioChartQuery } = dashboardApi;
+export const { useGetDashboardStatsQuery, useGetDashboardUserChartQuery, useGetDashboardEarningChartQuery } = dashboardApi;
